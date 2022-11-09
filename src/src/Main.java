daf63fba45291c7b6ed06e1eab3314a871793a11
Mahajan <Mudit.Mahajan@adidas.com>
2022-11-09 15:22:08 +0530

    functions and loops

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
+    public static void printHello() {
+        System.out.println("Hello");
+    }
+
+    public static void printTenNumbers(int[] paramArray) {
+        //Enhanced for loop, for each integer i in paramArray
+        //for (dataType item: array)
+        for (int i : paramArray) {
+            System.out.println(i + " ");
+        }
+    }
+
+    public static double salaryCalculator(int hoursWorked, double paidPerHour, int vacationDays) {
+        return (hoursWorked - vacationDays*8)*paidPerHour;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello");    //print out a statement
 
@@ -51,8 +68,32 @@ public class Main {
             }
         }
 
+        /*
+        Types of loops in java:
+        for/for each (enhanced for loop(Ref:9))
+        while
+        do...while
+         */
+
         //Debugging using print statements and debugging console in the top right corner
         //breakpoints: Left-click on the line number in IJ to insert a breakpoint(pause)
+        //program execution will pause on the above line
+
+        //Calling a function defined below
+        //Better to define the function before calling.
+        printHello();
+        //Calling a function with parameters
+        int[] nums = new int[10];
+        for (int i=0; i<nums.length; i++){
+            nums[i] = input.nextInt();
+        }
+        printTenNumbers(nums);
+
+        //Calculate salary
+        int hoursWorked = 40;
+        double paidPerHour = 10.0;
+        int vacationDays = 1;
+        System.out.println(salaryCalculator(hoursWorked, paidPerHour, vacationDays));
 
     }
 }
